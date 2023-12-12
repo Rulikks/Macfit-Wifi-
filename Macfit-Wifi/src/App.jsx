6bd606dbdae7868bb112a703d4845355533eefb1
Leora <cirem576@gmail.com>
2023-12-12 22:30:41 +0300

    success login screen

--- Macfit-Wifi/src/App.jsx
+++ Macfit-Wifi/src/App.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 import macfitLogo from './assets/macfit.png';
 import placeholderQRCode from './assets/frame.png';   
-import background from './assets/backgorund.jpg';
+import trainer from './assets/trainer.svg'
 
 function App() {
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [showQR, setShowQR] = useState(false); 
+  const [showQR, setShowQR] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false); // Eksik olan state tanımı
 
   const handleSubmit = (event) => {
     event.preventDefault(); 
     if (phoneNumber.length === 10) {
-      alert('Başarılı şekilde giriş yaptınız.');
+      setIsSubmitted(true); // Form başarılı bir şekilde gönderildi
     } else {
       alert('Lütfen geçerli bir telefon numarası giriniz. (10 haneli numara)');
     }
@@ -33,33 +34,44 @@ function App() {
           <img src={macfitLogo} alt="Macfit Logo" className="logo" />
           <div className="right-divider"></div>
         </div>
-        <h1>CONNECT TO WIFI</h1>    
+        {isSubmitted ? (
+          <div className="success-message">
+            Başarılı şekilde giriş yaptınız, Macfit İnternet ile hızına hız kat!
+            
+          </div>
+        ) : (
+          <>
 
-        <form className="wifi-form" onSubmit={handleSubmit}>
-          <input type="text" id="name" name="name" placeholder="İsim" required />
-          <input type="text" id="surname" name="surname" placeholder="Soyad" required />
-          <div className="phone-input">
-            <span className="phone-prefix">+90</span>
-            <input 
-              type="tel" 
-              id="phone" 
-              name="phone" 
-              placeholder="555 555 55 55" 
-              value={phoneNumber} 
-              onChange={handlePhoneChange} 
-              maxLength="10" 
-              required 
-            />
-          </div>
-          <button type="submit" className="submit-btn">Devam Et</button>
-        </form>
-        <div className="qr-text" onClick={toggleQRCode}>
-          OR YOU CAN USE QR CODE
-        </div>
-        {showQR && (
-          <div className="qr-code">
-            <img src={placeholderQRCode} alt="WiFi QR Code" />
-          </div>
+            <h1>CONNECT TO WIFI</h1>    
+
+            <form className="wifi-form" onSubmit={handleSubmit}>
+              <input type="text" id="name" name="name" placeholder="İsim" required />
+              <input type="text" id="surname" name="surname" placeholder="Soyad" required />
+              <div className="phone-input">
+                <span className="phone-prefix">+90</span>
+                <input 
+                  type="tel" 
+                  id="phone" 
+                  name="phone" 
+                  placeholder="555 555 55 55" 
+                  value={phoneNumber} 
+                  onChange={handlePhoneChange} 
+                  maxLength="10" 
+                  required 
+                />
+              </div>
+              <button type="submit" className="submit-btn">Devam Et</button>
+            </form>
+            <br></br>
+            <div className="qr-text" onClick={toggleQRCode}>
+              OR YOU CAN USE QR CODE
+            </div>
+            {showQR && (
+              <div className="qr-code">
+                <img src={placeholderQRCode} alt="WiFi QR Code" />
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
